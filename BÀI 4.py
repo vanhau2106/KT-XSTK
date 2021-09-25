6203bf0e950d275b6446e515a0e638823d242007
vanhau2106 <90501691+vanhau2106@users.noreply.github.com>
2021-09-25 15:51:15 +0700

    Add files via upload

---
+++ BÀI 4.py
@@ -0,0 +1,9 @@
+# NGUYỄN VĂN HẬU
+n= int(input(" nhập số nguyên dương n :"))
+def tinhtong(n):
+    a = 0
+    while (n>0):
+        a =a + n%10
+        n= int(n/10)
+    return a
+print("Tổng các chữ số của ",n, "là" ,tinhtong(n))
